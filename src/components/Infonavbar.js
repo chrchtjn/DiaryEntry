857f81b9d4454163a9015a864b5e6c32de1fb9c2
Charchit Jain <skyspark@Charchits-MacBook-Pro.local>
2023-02-14 17:00:18 +0530

    Added Infonavbar and Main navigation component

---
+++ src/components/Infonavbar.js
@@ -0,0 +1,23 @@
+import './Infonavbar.css';
+
+export default function Infonavbar(){
+    return(
+        <nav className="infonavbar">
+              
+            <ul className='out'>
+                 <ul className='in'>
+                        <li>
+                            14th November 2023
+                        </li>
+                        <li>
+                            3:42PM
+                        </li>
+                </ul>
+                <li>
+                    NAMEGOESHERE
+                </li>
+            </ul>
+            
+        </nav>
+    );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import './Navigation.css';
+import Infonavbar from './Infonavbar';
+
+export default function Navigation(){
+
+    return(
+        <div className='container'>
+            <Infonavbar/>
+        </div>
+    )
+}
