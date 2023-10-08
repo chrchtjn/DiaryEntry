4c0a5f47e7d32520c98765d55bf2e0dff2ed57e3
FMK2312 <fmkhandwala39@gmail.com>
2023-10-08 13:55:25 +0530

    removing commented code

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,12 @@
-import styles from "./App.module.css";
-import Navbar from "./components/Signup_Login/NavBar";
-import SignUp from "./components/Signup_Login/SignUp";
-import LogIn from "./components/Signup_Login/LogIn";
-import LeftNavigation  from "./components/LeftPane/LeftNavigation";
+import LeftNavigation from "./components/LeftPane/LeftNavigation";
 import WorkSpace from "./components/WorkSpace/WorkSpace";
-import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
-
-
 
 function App() {
-  const getD = async () => {
-    const response = await fetch('http://localhost:8080/getData');
-    const data  = await response.json();
-    
-    console.log(data.msg);
-    return data;
-    
-
-}
-
-getD().then((data) => {console.log(data.msg)});
-  // useEffect(() =>{
-  //   getD();
-  // },[]);
   return (
-<>
-    <LeftNavigation/>
-    <WorkSpace/></>
-  //  <div className={styles["checkIn"]}>
-    
-  //        <Navbar />
-  //         <Routes>
-  //            <Route exact path="/" element={<SignUp />} />
-  //            <Route exact path="/Login" element={<LogIn />} />
-  //         </Routes>
-  //   </div> 
-    
+    <>
+      <LeftNavigation />
+      <WorkSpace />
+    </>
   );
 }
 
